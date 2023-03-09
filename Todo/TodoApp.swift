84028ecbc08c2fb9588bad376258e7661e97a944
Jenn <j3nnkkin@gmail.com>
2023-03-09 20:44:10 +0900

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Jenn Park on 2023-03-09.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
